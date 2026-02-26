8dfffb1cd419f5033b900d483ab4e90b744972be
agustin <agustin2345678@gmail.com>
2026-02-25 23:50:44 -0300

    renombrado porcentaje a porcentajeDescuento y agregado fechas de vigencia en ValidarCuponResponse

--- src/main/java/unrn/dto/ValidarCuponResponse.java
+++ src/main/java/unrn/dto/ValidarCuponResponse.java
@@ -1,9 +1,13 @@
 package unrn.dto;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public record ValidarCuponResponse(
         boolean valido,
-        Float porcentaje,
+        Float porcentajeDescuento,
+        LocalDate vigenteDesde,
+        LocalDate vigenteHasta,
         String motivo
-) implements Serializable {}
+) implements Serializable {
+}
