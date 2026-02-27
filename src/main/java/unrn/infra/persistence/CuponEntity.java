e0c534145db38a8db5a2210ab17929195cbff818
batito <elbatimen@gmail.com>
2026-02-27 14:44:36 -0300

    ci

--- src/main/java/unrn/infra/persistence/CuponEntity.java
+++ src/main/java/unrn/infra/persistence/CuponEntity.java
@@ -30,6 +30,46 @@ public class CuponEntity {
 
     private Float porcentaje;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Float getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(Float porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
     public Cupon asDomain() {
         return new Cupon(id, nombre, fechaInicio, fechaFin, porcentaje);
     }
