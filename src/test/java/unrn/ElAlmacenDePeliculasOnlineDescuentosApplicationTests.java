38ffb21e7998fb5bfeec2034cbddc4b3c979edb7
batito <elbatimen@gmail.com>
2026-02-27 13:33:17 -0300

    ci

--- src/test/java/unrn/ElAlmacenDePeliculasOnlineDescuentosApplicationTests.java
+++ src/test/java/unrn/ElAlmacenDePeliculasOnlineDescuentosApplicationTests.java
@@ -1,13 +1,25 @@
 package unrn;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootTest
+@SpringBootTest(classes = ElAlmacenDePeliculasOnlineDescuentosApplicationTests.TestConfig.class)
 class ElAlmacenDePeliculasOnlineDescuentosApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Configuration
+	@EnableAutoConfiguration(exclude = {
+			org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+			org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+			org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class,
+			org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration.class
+	})
+	public static class TestConfig {
 	}
 
 }
